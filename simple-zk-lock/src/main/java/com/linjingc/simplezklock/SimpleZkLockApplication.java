98b109e0b169766c96fa88fa9ce903c841a06ad1
chenxiaocehng <583188551@qq.com>
2019-08-13 13:21:14 +0800

    初始化

---
+++ simple-zk-lock/src/main/java/com/linjingc/simplezklock/SimpleZkLockApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.simplezklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleZkLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleZkLockApplication.class, args);
+    }
+
+}
