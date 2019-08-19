412b3b0f76c00125fd8d86f80ef4aa0d87e28ef7
chenxiaocehng <583188551@qq.com>
2019-08-19 15:38:05 +0800

    初始化 使用注解和curator 内置的锁 InterProcessMutex：分布式可重入排它锁 InterProcessSemaphoreMutex：分布式排它锁 InterProcessReadWriteLock：分布式读写锁 InterProcessMultiLock：将多个锁作为单个实体管理的容器

---
+++ annotaion-curator-zk-lock/src/main/java/com/linjingc/annotaioncuratorzklock/AnnotaionCuratorZkLockApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.annotaioncuratorzklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotaionCuratorZkLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotaionCuratorZkLockApplication.class, args);
+    }
+
+}
