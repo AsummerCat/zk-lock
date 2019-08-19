87763bf7834aeb3a00e7501f4a0156dd875cbb31
chenxiaocehng <583188551@qq.com>
2019-08-19 15:18:45 +0800

    使用工厂类实例化curator

--- annotation-zk-lock/src/main/java/com/linjingc/annotationzklock/lock/config/CuratorConfiguration.java
+++ annotation-zk-lock/src/main/java/com/linjingc/annotationzklock/lock/config/CuratorConfiguration.java
@@ -16,27 +16,49 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CuratorConfiguration {
 
+    /**
+     * 重试次数
+     */
     @Value("${curator.retryCount}")
     private int retryCount;
-
+    /**
+     * 重试间隔时间
+     */
     @Value("${curator.elapsedTimeMs}")
     private int elapsedTimeMs;
-
+    /**
+     * zk地址 集群 逗号隔开
+     */
     @Value("${curator.connectString}")
     private String connectString;
 
+    /**
+     * 设定会话超时时间
+     */
     @Value("${curator.sessionTimeoutMs}")
     private int sessionTimeoutMs;
-
+    /**
+     * 设定连接时间超时
+     */
     @Value("${curator.connectionTimeoutMs}")
     private int connectionTimeoutMs;
 
     @Bean(initMethod = "start")
     public CuratorFramework curatorFramework() {
-        return CuratorFrameworkFactory.newClient(
-                connectString,
-                sessionTimeoutMs,
-                connectionTimeoutMs,
-                new RetryNTimes(retryCount, elapsedTimeMs));
+
+        return CuratorFrameworkFactory.builder()
+                // 放入zookeeper服务器ip
+                .connectString(connectString)
+                .sessionTimeoutMs(sessionTimeoutMs)
+                .connectionTimeoutMs(connectionTimeoutMs)
+                //curator链接zookeeper的策略:ExponentialBackoffRetry
+                .retryPolicy(new RetryNTimes(retryCount, elapsedTimeMs))
+                .build();
+
+//        return CuratorFrameworkFactory.newClient(
+//                connectString,
+//                sessionTimeoutMs,
+//                connectionTimeoutMs,
+//                new RetryNTimes(retryCount, elapsedTimeMs));
     }
 }
